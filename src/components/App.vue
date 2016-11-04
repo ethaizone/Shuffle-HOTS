7aa9f0e96b58846b4c6d99e5d7cc3e5244ce7d68
EThaiZone <ethaizone@hotmail.com>
2016-11-04 23:58:56 +0700

    remove some junk

--- src/components/App.vue
+++ src/components/App.vue
@@ -287,20 +287,9 @@ export default {
         player2: this.luckyPlayer
       })
     }
-  },
-  components: {
-    // Hello
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,15 +5,6 @@ import store from './store'
 
 Vue.use(VueTouch)
 
-Vue.directive('focus', {
-  bind: function () {
-    var object = this.el
-    Vue.nextTick(() => {
-      object.focus()
-    })
-  }
-})
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
